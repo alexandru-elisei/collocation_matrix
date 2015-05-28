c7a08d0f63e3a4b8524480ae6d0082a83378e795
Alexandru Elisei <alexandru.elisei@gmail.com>
2015-05-28 18:16:27 +0300

    wgraph_add now adds neighbours based on previous word index

--- main.c
+++ main.c
@@ -76,6 +76,7 @@ int main(int argc, char **argv)
 		fprintf(out, "%g\n", wgraph_cost_by_name(word_graph,
 			search_tree, cost_words[2*i], cost_words[2*i+1]));
 
+#if 0
 	for (i = 0; i < min_no; i++) {
 		min_path = wgraph_min_path(word_graph, search_tree, 
 				min_words[2*i], min_words[2*i+1], &min_path_len);
@@ -104,6 +105,7 @@ int main(int argc, char **argv)
 	for (i = 0; i < fixed_no; i++)
 		wgraph_fixed_path(word_graph, search_tree, 
 			fixed_lengths[i], fixed_words[i], &num_of_paths);
+#endif
 
 	//printf("destroying search_tree\n");
 	search_tree = tree_destroy(search_tree);
@@ -190,24 +192,25 @@ enum word_result parse_text(FILE *text, struct tnode **t,
 	char buffer[LINE_LEN];
 	char word[WORD_LEN];
 	/* The address is shared between the search tree and the graph */
-	char *word_shared_address, *previous_word;
+	char *word_address;
+	int prev_index;			/* index of previously inserted word */
 	int word_index;
 	int graph_index;
 	int i;
 
 	*total_words = 0;
-	previous_word = NULL;
-	word_shared_address = NULL;
+	word_address = NULL;
+	prev_index = graph_index = NODE_NOT_FOUND;
 	while (fgets(buffer, LINE_LEN, text) != NULL) {
 		word[0] = '\0';
 		word_index = 0;
 		for (i = 0; buffer[i] != '\0'; i++) {
-			/* All lower case */
-			if (buffer[i] >= 'A' && buffer[i] <= 'Z') {
-				word[word_index++] = buffer[i] - 'A' + 'a';
 			/* Lower case letters */
-			} else if (buffer[i] >= 'a' && buffer[i] <= 'z') {
+			if (buffer[i] >= 'a' && buffer[i] <= 'z') {
 				word[word_index++] = buffer[i];
+			/* Converting upper case to lower case */
+			} else if (buffer[i] >= 'A' && buffer[i] <= 'Z') {
+				word[word_index++] = buffer[i] - 'A' + 'a';
 			/* Digits */
 			} else if (buffer[i] >= '0' && buffer[i] <= '9') {
 				word[word_index++] = buffer[i];
@@ -216,24 +219,16 @@ enum word_result parse_text(FILE *text, struct tnode **t,
 				word[word_index++] = buffer[i];
 			/* Words have at least one letter/digit/' */
 			} else if (strchr(SEP, buffer[i]) != NULL &&
-					word[0] != '\0') {
+							word[0] != '\0') {
 				word[word_index] = '\0';
-				word_shared_address = strdup(word);
+				word_address = strdup(word);
 
-				/* 
-				 * Adding the previous word and its neighbour,
-				 * the current word, to the search tree and 
-				 * checking if it was found before in the text
-				 */
-				if (previous_word != NULL) {
-					graph_index = tree_add(t, previous_word, 
-							graph->size);
-					wgraph_add(graph, previous_word, 
-						word_shared_address, 
-						graph_index, COST_UNKNOWN);
-				}
-
-				previous_word = word_shared_address;
+				/* Adding the word */
+				graph_index = tree_add(t, word_address,
+						graph->size);
+				wgraph_add(graph, word_address, graph_index,
+					       &prev_index, COST_UNKNOWN);
+				prev_index = graph_index;
 
 				word[0] = '\0';
 				word_index = 0;
@@ -242,9 +237,10 @@ enum word_result parse_text(FILE *text, struct tnode **t,
 		}
 	}
 
-	/* Adding the last word */
-	graph_index = tree_add(t, word_shared_address, graph->size);
-	wgraph_add(graph, word_shared_address, NULL, graph_index, COST_UNKNOWN);
+	/* 
+	 * Storing the index of the last word, there's no word after it for the
+	 * o12 formula 
+	 */
 	graph->last_word_index = graph_index;
 
 	return WORD_SUCCESS;
